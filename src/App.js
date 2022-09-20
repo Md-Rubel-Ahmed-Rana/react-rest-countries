557fc1c66ad22a2ef7320a7633e177c1d46c70a2
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-21 00:35:26 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Countries from "./components/countries/Countries";
+
+
+function App() {
+  return (
+    <div className="App">
+     <Countries />
+    </div>
+  );
+}
+
+export default App;
